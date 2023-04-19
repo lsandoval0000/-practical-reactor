fbecb0c630672e793d3cb6c2106b74193233d9e6
Luis Sandoval <lsandoval0000@gmail.com>
2023-04-19 14:31:19 -0500

    .

--- exercises/src/test/java/c12_Broadcasting.java
+++ exercises/src/test/java/c12_Broadcasting.java
@@ -7,7 +7,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * In this chapter we will learn difference between hot and cold publishers,
- * how to split a publisher into multiple and how to keep history so late subscriber don't miss any updates.
+ * how to split a publisher into multiple and how to keep history so late
+ * subscriber don't miss any updates.
  *
  * Read first:
  *
@@ -26,16 +27,16 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class c12_Broadcasting extends BroadcastingBase {
 
     /**
-     * Split incoming message stream into two streams, one contain user that sent message and second that contains
+     * Split incoming message stream into two streams, one contain user that sent
+     * message and second that contains
      * message payload.
      */
     @Test
     public void sharing_is_caring() throws InterruptedException {
         Flux<Message> messages = messageStream()
-                //todo: do your changes here
-                ;
+                .publish().refCount(2);
 
-        //don't change code below
+        // don't change code below
         Flux<String> userStream = messages.map(m -> m.user);
         Flux<String> payloadStream = messages.map(m -> m.payload);
 
@@ -49,51 +50,53 @@ public class c12_Broadcasting extends BroadcastingBase {
 
         Assertions.assertEquals(Arrays.asList("user#0", "user#1", "user#2", "user#3", "user#4"), metaData);
         Assertions.assertEquals(Arrays.asList("payload#0", "payload#1", "payload#2", "payload#3", "payload#4"),
-                                payload);
+                payload);
     }
 
     /**
-     * Since two subscribers are interested in the updates, which are coming from same source, convert `updates` stream
+     * Since two subscribers are interested in the updates, which are coming from
+     * same source, convert `updates` stream
      * to from cold to hot source.
-     * Answer: What is the difference between hot and cold publisher? Why does won't .share() work in this case?
+     * Answer: What is the difference between hot and cold publisher? Why does won't
+     * .share() work in this case?
      */
     @Test
     public void hot_vs_cold() {
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
-                ;
+                .publish().autoConnect();
 
-        //subscriber 1
+        // subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
+                .verifyComplete();
 
-        //subscriber 2
+        // subscriber 2
         StepVerifier.create(updates.take(4).doOnNext(n -> System.out.println("subscriber 2 got: " + n)))
-                    .expectNext("DISK_SPACE_LOW", "OOM_DETECTED", "CRASHED", "UNKNOWN")
-                    .verifyComplete();
+                .expectNext("DISK_SPACE_LOW", "OOM_DETECTED", "CRASHED", "UNKNOWN")
+                .verifyComplete();
     }
 
     /**
-     * In previous exercise second subscriber subscribed to update later, and it missed some updates. Adapt previous
-     * solution so second subscriber will get all updates, even the one's that were broadcaster before its
+     * In previous exercise second subscriber subscribed to update later, and it
+     * missed some updates. Adapt previous
+     * solution so second subscriber will get all updates, even the one's that were
+     * broadcaster before its
      * subscription.
      */
     @Test
     public void history_lesson() {
         Flux<String> updates = systemUpdates()
-                //todo: do your changes here
-                ;
+                .replay().autoConnect();
 
-        //subscriber 1
+        // subscriber 1
         StepVerifier.create(updates.take(3).doOnNext(n -> System.out.println("subscriber 1 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY")
+                .verifyComplete();
 
-        //subscriber 2
+        // subscriber 2
         StepVerifier.create(updates.take(5).doOnNext(n -> System.out.println("subscriber 2 got: " + n)))
-                    .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY", "DISK_SPACE_LOW", "OOM_DETECTED")
-                    .verifyComplete();
+                .expectNext("RESTARTED", "UNHEALTHY", "HEALTHY", "DISK_SPACE_LOW", "OOM_DETECTED")
+                .verifyComplete();
     }
 
 }
--- exercises/src/test/java/c13_Context.java
+++ exercises/src/test/java/c13_Context.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 import reactor.test.StepVerifier;
 import reactor.util.context.Context;
 
@@ -8,8 +9,10 @@ import java.time.Duration;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Often we might require state when working with complex streams. Reactor offers powerful context mechanism to share
- * state between operators, as we can't rely on thread-local variables, because threads are not guaranteed to be the
+ * Often we might require state when working with complex streams. Reactor
+ * offers powerful context mechanism to share
+ * state between operators, as we can't rely on thread-local variables, because
+ * threads are not guaranteed to be the
  * same. In this chapter we will explore usage of Context API.
  *
  * Read first:
@@ -27,68 +30,85 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class c13_Context extends ContextBase {
 
     /**
-     * You are writing a message handler that is executed by a framework (client). Framework attaches a http correlation
-     * id to the Reactor context. Your task is to extract the correlation id and attach it to the message object.
+     * You are writing a message handler that is executed by a framework (client).
+     * Framework attaches a http correlation
+     * id to the Reactor context. Your task is to extract the correlation id and
+     * attach it to the message object.
      */
     public Mono<Message> messageHandler(String payload) {
-        //todo: do your changes withing this method
-        return Mono.just(new Message("set correlation_id from context here", payload));
+        return Mono.deferContextual(ctx -> {
+            String correlatedId = ctx.get(HTTP_CORRELATION_ID);
+            return Mono.just(new Message(correlatedId, payload));
+        });
     }
 
     @Test
     public void message_tracker() {
-        //don't change this code
+        // don't change this code
         Mono<Message> mono = messageHandler("Hello World!")
                 .contextWrite(Context.of(HTTP_CORRELATION_ID, "2-j3r9afaf92j-afkaf"));
 
         StepVerifier.create(mono)
-                    .expectNextMatches(m -> m.correlationId.equals("2-j3r9afaf92j-afkaf") && m.payload.equals(
-                            "Hello World!"))
-                    .verifyComplete();
+                .expectNextMatches(m -> m.correlationId.equals("2-j3r9afaf92j-afkaf") && m.payload.equals(
+                        "Hello World!"))
+                .verifyComplete();
     }
 
     /**
-     * Following code counts how many times connection has been established. But there is a bug in the code. Fix it.
+     * Following code counts how many times connection has been established. But
+     * there is a bug in the code. Fix it.
      */
     @Test
     public void execution_counter() {
         Mono<Void> repeat = Mono.deferContextual(ctx -> {
             ctx.get(AtomicInteger.class).incrementAndGet();
             return openConnection();
-        });
-        //todo: change this line only
-        ;
+        }).contextWrite(Context.of(AtomicInteger.class, new AtomicInteger(0)));
 
         StepVerifier.create(repeat.repeat(4))
-                    .thenAwait(Duration.ofSeconds(10))
-                    .expectAccessibleContext()
-                    .assertThat(ctx -> {
-                        assert ctx.get(AtomicInteger.class).get() == 5;
-                    }).then()
-                    .expectComplete().verify();
+                .thenAwait(Duration.ofSeconds(10))
+                .expectAccessibleContext()
+                .assertThat(ctx -> {
+                    assert ctx.get(AtomicInteger.class).get() == 5;
+                }).then()
+                .expectComplete().verify();
     }
 
     /**
      * You need to retrieve 10 result pages from the database.
      * Using the context and repeat operator, keep track of which page you are on.
-     * If the error occurs during a page retrieval, log the error message containing page number that has an
+     * If the error occurs during a page retrieval, log the error message containing
+     * page number that has an
      * error and skip the page. Fetch first 10 pages.
      */
     @Test
     public void pagination() {
-        AtomicInteger pageWithError = new AtomicInteger(); //todo: set this field when error occurs
+        AtomicInteger pageWithError = new AtomicInteger(); // todo: set this field when error occurs
 
-        //todo: start from here
-        Flux<Integer> results = getPage(0)
+        // todo: start from here
+        Flux<Integer> results = Mono.deferContextual(ctx -> getPage(ctx.get(AtomicInteger.class).get()))
+                .doOnEach(signal -> {
+                    if (signal.getType() == SignalType.ON_NEXT) {
+                        signal.getContextView().get(AtomicInteger.class).incrementAndGet();
+                    } else if (signal.getType() == SignalType.ON_ERROR) {
+                        pageWithError.set(signal.getContextView().get(AtomicInteger.class).get());
+                        System.out.println(
+                                "Error has occurred: " + signal.getThrowable().getMessage());
+                        System.out.println("Error occurred at page: " + signal.getContextView()
+                                .get(AtomicInteger.class)
+                                .getAndIncrement());
+                    }
+                })
+                .onErrorResume(e -> Mono.empty())
                 .flatMapMany(Page::getResult)
                 .repeat(10)
-                .doOnNext(i -> System.out.println("Received: " + i));
+                .doOnNext(i -> System.out.println("Received: " + i))
+                .contextWrite(Context.of(AtomicInteger.class, new AtomicInteger(0)));
 
-
-        //don't change this code
+        // don't change this code
         StepVerifier.create(results)
-                    .expectNextCount(90)
-                    .verifyComplete();
+                .expectNextCount(90)
+                .verifyComplete();
 
         Assertions.assertEquals(3, pageWithError.get());
     }
